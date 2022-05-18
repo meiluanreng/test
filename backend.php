b77c9b2f1c58ef6b9df144bf6a102bc2e23f373f
meiluanreng <93702555+meiluanreng@users.noreply.github.com>
2022-05-17 22:49:31 -0700

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+
+Welcome
+
+<?php echo $_POST["name"]; ?> <p>Your email is: <?php echo $_POST["email"]; ?>
+
+<p>Your URL is: <?php echo $_POST["url"]; ?>
+
+<p>Your phone Number is: <?php echo $_POST["phone"]; ?>
+
+<p>The best time to reach you is: <?php echo $_POST["time"]; ?>
+
+</body>
+
+</html>
